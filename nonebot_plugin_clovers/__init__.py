242a751a29d0930471d0f5d1c9c290e5f59034b5
KarisAya <1048827424@qq.com>
2025-01-30 22:06:48 +0800

    使用nonebot_plugin_localstore

--- nonebot_plugin_clovers/__init__.py
+++ nonebot_plugin_clovers/__init__.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from nonebot import get_driver
+from nonebot import get_driver, require
 from nonebot.plugin import PluginMetadata
 from nonebot.log import LoguruHandler
+from pathlib import Path
 from clovers import Clovers
 from clovers.tools import list_modules
 from clovers.logger import logger as clovers_logger
 from clovers.config import config as clovers_config
-from .config import ConfigClovers
 
 __plugin_meta__ = PluginMetadata(
     name="clovers插件框架",
@@ -24,15 +24,29 @@ log_level = driver.config.log_level
 clovers_logger.setLevel(log_level)
 clovers_logger.addHandler(LoguruHandler(log_level))
 
+require("nonebot_plugin_localstore")
+
+import nonebot_plugin_localstore as store
+
+
 # 加载 Clovers配置
 config_key = "clovers"
-clovers_config_data = ConfigClovers.model_validate(clovers_config.get(config_key, {}))
-clovers_config[config_key] = clovers_config_data.model_dump()
+clovers_config[config_key] = clovers_config.get(config_key, {})
 
 # 加载 NoneBot 配置
 nonebot_config = driver.config
 command_start = [x for x in nonebot_config.command_start if x]
 Bot_NICKNAME = Bot_NICKNAMEs[0] if (Bot_NICKNAMEs := list(nonebot_config.nickname)) else "bot"
+
+clovers_config[config_key]["Bot_NICKNAME"] = Bot_NICKNAME
+
+plugins: list[str] = clovers_config[config_key].setdefault("plugins", [])
+adapters: list[str] = clovers_config[config_key].setdefault("adapters", [])
+
+clovers_library = store.get_data_dir("clovers")
+
+plugin_dirs: list[str] = clovers_config[config_key].setdefault("plugin_dirs", [str(clovers_library / "plugins")])
+adapters_dirs: list[str] = clovers_config[config_key].setdefault("adapters_dirs", [str(clovers_library / "adapters")])
 
 if command_start:
 
@@ -54,13 +68,13 @@ async def _():
 
 
 # 初始化
-clovers.load_adapters(clovers_config_data.adapters)
-for dir in clovers_config_data.adapters_dirs:
+clovers.load_adapters(adapters)
+for dir in adapters_dirs:
     Path(dir).mkdir(exist_ok=True, parents=True)
     clovers.load_adapters(list_modules(dir))
 
 
-clovers.load_plugins(clovers_config_data.plugins)
-for dir in clovers_config_data.plugin_dirs:
+clovers.load_plugins(plugins)
+for dir in plugin_dirs:
     Path(dir).mkdir(exist_ok=True, parents=True)
     clovers.load_plugins(list_modules(dir))
--- nonebot_plugin_clovers/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ConfigClovers(BaseModel):
-    plugins: list[str] = []
-    plugin_dirs: list[str] = ["./clovers_library/plugins"]
-    adapters: list[str] = []
-    adapters_dirs: list[str] = ["./clovers_library/adapters"]
