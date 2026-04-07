9097a80ffcc929fc64ddb2d5afa82abab150571c
KarisAya <karisaya@foxmail.com>
2026-04-07 21:22:38 +0800

    升级

--- nonebot_plugin_clovers/clovers.py
+++ nonebot_plugin_clovers/clovers.py
@@ -2,7 +2,6 @@ from functools import wraps
 from nonebot.adapters import Bot, Event
 from nonebot.matcher import Matcher
 from clovers import CloversCore
-from clovers.core import ModuleLoader
 from typing import Protocol, runtime_checkable
 
 
@@ -15,10 +14,10 @@ class NoneBotCore(CloversCore):
     def __init__(self, adapter_name: str, bot_name: str, plugin_list: list[str], plugin_dirs: list[str], name: str = "NoneBotClovers"):
         super().__init__(name)
         self.BOT_NICKNAME = bot_name
-        self.handler = wraps(ModuleLoader.load(adapter_name, ["handler"], Handler))(self._handler)
         self.adapter.property_method("bot_nickname")(self.bot_name)
         self.load_adapter([adapter_name])
         self.load_plugin(plugin_list, plugin_dirs)
+        self.handler = wraps(self.adapter.calls_lib["none"])(self.handler)
 
     async def bot_name(self) -> str:
         return self.BOT_NICKNAME
@@ -26,6 +25,6 @@ class NoneBotCore(CloversCore):
     def extract_message(self, event: Event, **ignore) -> str | None:
         return event.get_plaintext()
 
-    async def _handler(self, bot, event, matcher):
+    async def handler(self, bot, event, matcher):
         if (coro := self.dispatch(bot=bot, event=event)) and await coro:
             matcher.stop_propagation()
