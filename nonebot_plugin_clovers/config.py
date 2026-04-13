f75a65380255faa0b4290ff244573e5292e94e27
KarisAya <karisaya@foxmail.com>
2026-04-13 21:17:15 +0800

    升级

--- nonebot_plugin_clovers/config.py
+++ nonebot_plugin_clovers/config.py
@@ -2,7 +2,7 @@ from clovers_client import Config as CloversConfig
 
 
 class ScopedConfig(CloversConfig):
-    using_adapters: list[str] = ["~onebot.v11", "~qq.group", "~qq.guild", "~satori", "~uninfo"]
+    using_adapters: list[str] = ["~onebot.v11", "~satori", "~uninfo"]
     priority: int = 100
     plugins: list[str] = []
     plugin_dirs: list[str] = []
